fd7e8bec0e9a29875b1c5f47a2c6a8c62e930332
MarDi96 <martin_dilella@hotmail.com>
2025-04-22 21:40:56 -0300

    Se agregaron playlists La pantalla de opciones ahora tiene un botón de conseguir permisos Se redujo el input lag

--- app/src/main/java/com/plcoding/videoplayercompose/Options.kt
+++ app/src/main/java/com/plcoding/videoplayercompose/Options.kt
@@ -1,16 +1,20 @@
 package com.plcoding.videoplayercompose
 
-import android.graphics.Color
-import android.graphics.Paint.Align
+
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,9 +22,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.graphics.Color
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Options(){
+fun Options() {
     var showDialog by remember { mutableStateOf(false) }
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
@@ -32,34 +40,56 @@ fun Options(){
             }
         }
     )
-    Box(modifier = Modifier.fillMaxSize()){
-        Column(modifier = Modifier.fillMaxSize()
-            .align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally){
-            Button(onClick = {
-                launcher.launch(arrayOf(
-                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    android.Manifest.permission.READ_MEDIA_AUDIO,
-                    android.Manifest.permission.POST_NOTIFICATIONS
-                ))
-            }) {
-                Text("Grant Permissions")
-            }
+
+    Scaffold(
+        containerColor = if (isSystemInDarkTheme()) Color.Transparent else Color.Transparent,
+        topBar = {
+            SmallTopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray,
+                    titleContentColor = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
+                ),
+                title = { Text("Options") }
+            )
         }
-    }
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text("Permissions Granted") },
-            text = { Text("Please restart the app for the changes to make effect") },
-            confirmButton = {
-                Button(onClick = { showDialog = false }) {
-                    Text("OK")
+    ) { innerPadding ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.Center),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = {
+                    launcher.launch(
+                        arrayOf(
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            android.Manifest.permission.READ_MEDIA_AUDIO,
+                            android.Manifest.permission.POST_NOTIFICATIONS
+                        )
+                    )
+                }) {
+                    Text("Grant Permissions")
                 }
             }
-        )
+        }
+
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = { Text("Permissions Granted") },
+                text = { Text("Please restart the app for the changes to take effect") },
+                confirmButton = {
+                    Button(onClick = { showDialog = false }) {
+                        Text("OK")
+                    }
+                }
+            )
+        }
     }
 }
 
